2daa0b4d825bd8fbb1d22db6cfff58147d911284
chesterboy01 <chesterboy01@126.com>
2016-10-08 14:47:26 -0400

    first_commit20161008

---
+++ app/src/main/java/io/github/chesterboy01/freex/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package io.github.chesterboy01.freex;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, FullscreenActivityBeforeLogin.class);
+                startActivity(intent);
+                WelcomeActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
